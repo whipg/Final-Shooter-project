037c862b5ae80b18a67f4f09fbb25217592df8ef
Harvey Hutchings-Smith <harveyhutchingssmith@gmail.com>
2015-06-10 19:40:37 +1000

    Added Delta Time

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
+var canvas = document.getElementById("gameCanvas");
+var context = canvas.getContext("2d");
 
+var startFrameMillis = Date.now();
+var endFrameMillis = Date.now();
+ 
+function getDeltaTime()
+{
+        endFrameMillis = startFrameMillis;
+        startFrameMillis = Date.now();
+ 
+                // Find the delta time (dt) - the change in time since the last drawFrame
+                // We need to modify the delta time to something we can use.
+                // We want 1 to represent 1 second, so if the delta is in milliseconds
+                // we divide it by 1000 (or multiply by 0.001). This will make our
+                // animations appear at the right speed, though we may need to use
+                // some large values to get objects movement and rotation correct
+        var deltaTime = (startFrameMillis - endFrameMillis) * 0.001;
+       
+                // validate that the delta is within range
+        if(deltaTime > 1)
+                deltaTime = 1;
+               
+        return deltaTime;
+}
  
 //-------------------- Don't modify anything below here
 // This code will set up the framework so that the 'run' function is
