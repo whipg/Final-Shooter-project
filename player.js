8c82c0bc1278f794fdb72f3db1fc64e972a2e72c
OwenGunter <owen.gunter@student.temple.sa.edu.au>
2015-06-10 19:06:27 +0930

    player.js
    player.js

---
+++ player.js
@@ -0,0 +1,13 @@
+var Player = function() {
+this.image = document.createElement("img");
+this.x = canvas.width/2;
+this.y = canvas.height/2;
+this.width = 159;
+this.height = 163;
+this.velocityX = 0;
+this.velocityY = 0;
+this.angularVelocity = 0;
+this.rotation = 0;
+this.image.src = "hero.png";
+};
+var player = new Player();
