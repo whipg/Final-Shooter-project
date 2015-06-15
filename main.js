31dd0a6842786ce6ded25c8c33c8a157636d219d
Harvey Hutchings-Smith <harveyhutchingssmith@gmail.com>
2015-06-15 19:59:14 +1000

    fix

--- main.js
+++ main.js
@@ -45,8 +45,6 @@ function run()
 
     var deltaTime = getDeltaTime();
 
-<<<<<<< HEAD
-	
 	for(var i=0; i<bullets.length; i++)
 	{
 		bullets[i].update(deltaTime);
@@ -54,8 +52,7 @@ function run()
 	{
 		hit = true;
 	}
-	
-=======
+
     if(keyboard.isKeyDown(keyboard.KEY_SHOOT) == true)
     {
     context.fillStyle = "#F02936";
@@ -63,7 +60,6 @@ function run()
     context.fillText("Click!",SCREEN_WIDTH - 170, 35);
     }
 
->>>>>>> origin/master
     player.update(deltaTime);
     player.draw();
 	context.drawImage(player.image, SCREEN_WIDTH/2 - player.width/2, SCREEN_HEIGHT/2 - player.height/2);
