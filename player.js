fc364957fe732f422fd446c10969348e2f63ca73
Harvey Hutchings-Smith <harveyhutchingssmith@gmail.com>
2015-06-29 20:52:24 +1000

    stuff

--- player.js
+++ player.js
@@ -4,89 +4,78 @@ var RIGHT = 1;
 var UP = 2;
 var DOWN = 3;
 
-var Player = function(){
-this.sprite = new Sprite("playerplaceholder.png");
-this.sprite.buildAnimation(1, 1, 32, 32, -1, [0]);
-this.sprite.setAnimationOffset(0, 0, 0);
-this.sprite.setLoop(0, false);
+var Player = function() {
+  this.sprite = new Sprite("playerplaceholder.png");
+  this.sprite.buildAnimation(1, 1, 32, 32, -1, [0]);
+  this.sprite.setAnimationOffset(0, 0, 0);
+  this.sprite.setLoop(0, false);
 
-this.position = new Vector2();
-this.position.set(200, 200);
+  this.position = new Vector2();
+  this.position.set(200, 200);
 
-this.width = 48;
-this.height = 48;
+  this.width = 48;
+  this.height = 48;
 
-this.moveVector = new Vector2();
+  this.moveVector = new Vector2();
 };
 
 
-Player.prototype.update = function(deltaTime)
-{
-	this.sprite.update(deltaTime);
+Player.prototype.update = function(deltaTime) {
+  this.sprite.update(deltaTime);
 
-	this.moveVector.x = 0;
-	this.moveVector.y = 0;
+  this.moveVector.x = 0;
+  this.moveVector.y = 0;
 
-	var left = false;
-	var right = false;
-	var up = false;
-	var down = false;
+  var left = false;
+  var right = false;
+  var up = false;
+  var down = false;
 
-	if((keyboard.isKeyDown(keyboard.KEY_LEFT)) != (keyboard.isKeyDown(keyboard.KEY_RIGHT)))
-	{
-		if(keyboard.isKeyDown(keyboard.KEY_RIGHT))
-		{
-			right = true;
-		}
-		else if(keyboard.isKeyDown(keyboard.KEY_LEFT))
-		{
-			left = true;
-		}
-	}
-	if((keyboard.isKeyDown(keyboard.KEY_UP)) != (keyboard.isKeyDown(keyboard.KEY_DOWN)))
-	{
-		if(keyboard.isKeyDown(keyboard.KEY_DOWN))
-		{
-			down = true;
-		}
-		else if(keyboard.isKeyDown(keyboard.KEY_UP))
-		{
-			up = true;
-		}
-	}
+  if ((keyboard.isKeyDown(keyboard.KEY_LEFT)) != (keyboard.isKeyDown(keyboard.KEY_RIGHT))) {
+    if (keyboard.isKeyDown(keyboard.KEY_RIGHT)) {
+      right = true;
+    } else if (keyboard.isKeyDown(keyboard.KEY_LEFT)) {
+      left = true;
+    }
+  }
+  if ((keyboard.isKeyDown(keyboard.KEY_UP)) != (keyboard.isKeyDown(keyboard.KEY_DOWN))) {
+    if (keyboard.isKeyDown(keyboard.KEY_DOWN)) {
+      down = true;
+    } else if (keyboard.isKeyDown(keyboard.KEY_UP)) {
+      up = true;
+    }
+  }
 
-	var wasleft = this.moveVector.x < 0;
-	var wasright = this.moveVector.x > 0;
-	var wasdown = this.moveVector.y > 0;
-	var wasup = this.moveVector.y < 0;
+  var wasleft = this.moveVector.x < 0;
+  var wasright = this.moveVector.x > 0;
+  var wasdown = this.moveVector.y > 0;
+  var wasup = this.moveVector.y < 0;
 
-	if(right)
-	this.moveVector.x = 50 * deltaTime;
-	else if (wasright)
-	this.moveVector.x = 0;
-	if(left)
-	this.moveVector.x = -50 * deltaTime;
-	else if (wasleft)
-	this.moveVector.x = 0;
-	if(down)
-	this.moveVector.y = 50 * deltaTime;
-	else if (wasdown)
-	this.moveVector.y = 0;
-	if(up)
-	this.moveVector.y = -50 * deltaTime;
-	else if (wasup)
-	this.moveVector.y = 0;
+  if (right)
+    this.moveVector.x = 50 * deltaTime;
+  else if (wasright)
+    this.moveVector.x = 0;
+  if (left)
+    this.moveVector.x = -50 * deltaTime;
+  else if (wasleft)
+    this.moveVector.x = 0;
+  if (down)
+    this.moveVector.y = 50 * deltaTime;
+  else if (wasdown)
+    this.moveVector.y = 0;
+  if (up)
+    this.moveVector.y = -50 * deltaTime;
+  else if (wasup)
+    this.moveVector.y = 0;
 
 
-	this.position.x += this.moveVector.x;
-	this.position.y += this.moveVector.y;
-
+  this.position.x += this.moveVector.x;
+  this.position.y += this.moveVector.y;
 
 
 
 }
 
-Player.prototype.draw = function()
-{
-	this.sprite.draw(context, this.position.x, this.position.y);
+Player.prototype.draw = function() {
+  this.sprite.draw(context, this.position.x, this.position.y);
 }
