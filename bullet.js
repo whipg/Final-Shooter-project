f5be5c9d3016f8c1e6d04815a9055334b7ec04ad
Harvey Hutchings-Smith <harveyhutchingssmith@gmail.com>
2015-07-01 20:28:56 +1000

    bullet updates

--- bullet.js
+++ bullet.js
@@ -1,35 +1,30 @@
-var bullet = {
-	this.x = Player.position.x
-	this.y = Player.position.y
-	this.width: 5
-	this.height: 5
-	this.velocityX: 0
-	this.velocityY: 0
+var Bullet = function(x, y, moveRight) {
+	this.sprite = new Sprite("bullet.png");
+	this.sprite.buildAnimation(1, 1, 32, 32, -1, [0]);
+	this.sprite.setAnimationOffset(0, 0, 0);
+	this.sprite.setLoop(0, false);
+
+	this.position = new Vector2();
+	this.position.set(x,y);
+
+	this.velocity = new Vector2();
+
+	var BULLET_SPEED = 450;
+
+	this.moveRight = moveRight;
+	this.moveLeft = true;
+	if(this.moveRight == true)
+		this.velocity.set(-MAXDX *2, 0);
+	else
+		this.velocity.set(MAXDX *2, 0);
 };
 
-bullet.image.src = "bullet.png";
-
-function playerShoot()
+Bullet.prototype.update = function(deltaTime)
 {
+	this.position.x = Math.floor(this.position.x + (deltaTime * this.velocity.x));
+}
 
-	if( bullet.isDead == false )
-	return;
-
-	var velX = 0;
-	var velY = 1;
-
-	var s = Math.sin(player.rotation);
-	var c = Math.cos(player.rotation);
-
-	var xVel = (velX * c) - (velY * s);
-	var yVel = (velX * s) + (velY * c);
-
-
-	bullet.velocityX = xVel * BULLET_SPEED;
-	bullet.velocityY = yVel * BULLET_SPEED;
-
-	bullet.x = player.x;
-	bullet.y = player.y;
-
-	bullet.isDead = false;
+Bullet.prototype.draw = function()
+{
+	this.sprite.draw(context, screenX, this.position.y);
 }
