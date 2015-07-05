eba23efaa4b822882b0604024ba8f849b5334a21
Harvey Hutchings-Smith <harveyhutchingssmith@gmail.com>
2015-07-05 20:29:39 +1000

    cats

--- enemy.js
+++ enemy.js
@@ -1,8 +1,9 @@
 var ANIM_WALK = 0;
 
 var Enemy = function() {
-  this.sprite = new Sprite("cat.png");
-  this.sprite.buildAnimation(4, 2, 498, 292, 0.05, [0,1,2,3,4,5,6,7]);
+  this.sprite = new Sprite("animals-cat.png");
+  //this.sprite.buildAnimation(4, 2, 498, 292, 0.05, [0,1,2,3,4,5,6,7]);
+  this.sprite.buildAnimation(1, 1, 133, 100, -1, [0]);
   this.sprite.setAnimationOffset(0, 0, 0);
   this.sprite.setLoop(0, false);
 
